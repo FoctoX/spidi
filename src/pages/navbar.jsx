8d2f8c88d1191bbc9d16f07904f77ccdf0c5d8bc
FoctoX <fabiogmg1410@gmail.com>
2024-01-10 17:46:55 +0700

    letest progress

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,10 +1,17 @@
 const Navbar = () => {
     return (
         <>
-            <div className="w-full h-10">
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="w-full h-20 bg-primary text-primary-text sticky shadow-lg">
+                <div className="h-full mx-3 flex justify-between items-center">
+                    <div className="text-5xl">Spidi.</div>
+                    <div className="flex">
+                        <p className="mx-2 p-4">Gujarat</p>
+                        <p className="mx-2 p-4">Persia</p>
+                        <p className="mx-2 p-4">India</p>
+                        <p className="mx-2 p-4">China</p>
+                    </div>
+                    <div className="">About Our</div>
+                </div>
             </div>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
+    colors: {
+      'primary': '#1E1E3A',
+      'primary-text': '#FFFFFF',
+      'secondary-text': '#dbdbdb'
+    },
     extend: {},
   },
   plugins: [],
 }
-
