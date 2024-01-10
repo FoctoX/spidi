3577969ad81d14f5d636ac92a54721a208bd3efa
FoctoX <fabiogmg1410@gmail.com>
2024-01-10 16:52:00 +0700

    Perubahan ke tailwind

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,13 +1,11 @@
 const Navbar = () => {
     return (
         <>
-            <nav className="navbar bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <b>Spidi.</b>
-                    </a>
-                </div>
-            </nav>
+            <div className="w-full h-10">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </>
     )
 }
